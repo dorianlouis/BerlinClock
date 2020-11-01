309ef2a918bfc4471eb776d702ea072bcefac0c3
Adam El M'Rabet <adam.elmrabet@student.vinci.be>
2020-11-01 14:49:16 +0100

    initial commit + add - calculateMinutes and test for 12H00

---
+++ BerlinClock.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class BerlinClock
+{
+    public function calculateMinutes($timer){
+        $var=explode(":",$timer);
+        $minutes = strval($var[1]) ;
+        $mod = $minutes%5;
+        $string ="";
+        for ($i =0; $i<$mod;$i++) {
+            $string .= "Y";
+        }
+        return $string;
+    }
+}
---
+++ BerlinClockTest.php
@@ -0,0 +1,19 @@
+<?php
+require "vendor/autoload.php";
+require "BerlinClock.php";
+use BerlinClock;
+use \PHPUnit\Framework\TestCase;
+
+class BerlinClockTest extends TestCase
+{
+    //TEST SIMPLES MINUTES
+
+    public function test_CalculateMinutes_given12H00_returnNothing(){
+        $test = new BerlinClock();
+        $timer="12:00:00";
+        $actual = $test->calculateMinutes($timer);
+        $this->assertEquals("",$actual);
+    }
+
+
+}
